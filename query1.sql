3cd978774bdb8aed4065e95c56a2fa7c1707f01c
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-14 21:53:08 +0700

    feat: add query1 task

--- query1.sql
+++ query1.sql
@@ -1,12 +1,15 @@
---SELECT * FROM "movies";
-
 -- 1. search diatas tahun 2000
-SELECT * FROM "movies" WHERE "year" >= 2000 ORDER BY "year" ASC;
+SELECT "id", "name", "year" FROM "movies" WHERE "year" >= 2000 ORDER BY "year" ASC;
 
 -- 2. search movie diantara rating 5 >= 7 dan rilis 2004 >= 2006
+SELECT "id", "name", "rankscore" FROM "movies" 
+WHERE "rankscore" >= 5 AND "rankscore" <= 7 
+AND "year" >= 2004 AND "year" <= 2006; 
 
 -- 3. menghitung jumlah movie dengan rating 6
+SELECT COUNT(*) AS "total", "rankscore" FROM "movies" WHERE "rankscore" = 6 GROUP BY "rankscore";
 
 -- 4. search actor dengan akhiran nama 's'
-SELECT * FROM "actors"; WHERE last_name LIKE '%s';
+SELECT "id", "first_name", "last_name", "gender" 
+FROM "actors" WHERE "last_name" LIKE '%s';
 
